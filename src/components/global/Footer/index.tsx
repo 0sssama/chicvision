520d3f44c1139ebef1f01c6eb29665d284d60a4a
Oussama Labrahmi <olabrahm@e2r4p2.1337.ma>
2022-02-23 21:07:07 +0100

    :gear: footer carousel

--- src/components/global/Footer/index.tsx
+++ src/components/global/Footer/index.tsx
@@ -1,16 +1,45 @@
 import Logo from "@/components/global/Header/Logo";
 import Link from "next/link";
 import { BsFacebook, BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 
-type Props = {};
+interface cardProps {
+  data: {
+    icon: string;
+    title: string;
+    content: string;
+  };
+}
 
-function Footer({}: Props) {
+function Card({ data }: cardProps) {
+  return (
+    <div className="Card flex flex-col items-start">
+      <img src={data.icon} alt={data.title} />
+      <h6 className="title secondary">{data.title}</h6>
+      <article className="text">{data.content}</article>
+    </div>
+  );
+}
+
+function Footer({}) {
   return (
     <div className="Footer w-full">
       <div className="Footer-carousel w-full">
         <div className="wrapper padding-x">
           <h4 className="title secondary">Pourquoi Chic Vision?</h4>
-          <p>Carousel here.</p>
+          <div className="Footer-carousel-wrapper w-full">
+            {cards.map((card, i) => (
+              <Card data={card} />
+            ))}
+          </div>
+          <div className="Footer-carousel-navigation w-full">
+            <div className="item previous disabled">
+              <AiOutlineLeft />
+            </div>
+            <div className="item next">
+              <AiOutlineRight />
+            </div>
+          </div>
         </div>
       </div>
       <div className="Footer-bottom w-full">
@@ -52,3 +81,36 @@ function Footer({}: Props) {
 }
 
 export default Footer;
+
+const cards = [
+  {
+    icon: "/assets/icons/gem.svg",
+    title: "Toujours près de vous",
+    content:
+      "Nous somme toujour a votre cote pour vous aider et a collaborer ensemble pour assurer la continuite de votre succes.",
+  },
+  {
+    icon: "/assets/icons/happy.svg",
+    title: "Le Service",
+    content:
+      "L'équipe de vente professionnelle fournira toujours des solutions tout-en-un pour votre entreprise de lunettes et vous aidera à générer plus de revenus de vente et d'aller avec votre business au next level.",
+  },
+  {
+    icon: "/assets/icons/gem.svg",
+    title: "Toujours près de vous",
+    content:
+      "Nous somme toujour a votre cote pour vous aider et a collaborer ensemble pour assurer la continuite de votre succes.",
+  },
+  {
+    icon: "/assets/icons/medal.svg",
+    title: "Partenariat",
+    content:
+      "Un partenariat commercial stratégique à long terme, y compris un soutien financier, une excellente communication et un investissement garantissant la croissance rapide de votre entreprise de lunettess.",
+  },
+  {
+    icon: "/assets/icons/secure.svg",
+    title: "Voir c'est croire!",
+    content:
+      "Lunettes de soleil et lunettes de lux en métal et acétate avec visite vidéo appelez-nous pour prendre rendez-vous maintenant! Expériences 100% garanties.",
+  },
+];
