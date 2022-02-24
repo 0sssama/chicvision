659b8dfb86a289a06a3b95dd2c4bfbb66f52ba20
Oussama Labrahmi <olabrahm@e2r9p14.1337.ma>
2022-02-24 08:27:48 +0100

    :gear: fixing build issues

--- src/pages/api/brands.ts
+++ src/pages/api/brands.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import brands from "@/data/brands.json";
 
-export default async (
+export default async function brandsRoute(
   _req: NextApiRequest,
   res: NextApiResponse
-): Promise<void> => {
+): Promise<void> {
   res.status(200).json(brands);
-};
+}
